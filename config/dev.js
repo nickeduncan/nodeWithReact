5303af79bb249b4b3e078b031ade215e640fa421
Nicholas Duncan <nickeduncan@gmail.com>
2018-06-20 10:22:26 -0400

    [5-47] moved keys to dev.js

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+// dev.js - don't commit this!!
+module.exports = {
+	googleClientID: '339143140949-qa883l7l6jbktrisqojo7v5fn87eraf6.apps.googleusercontent.com',
+	googleClientSecret: 'pheqSN1PQFpjz2x6M2586D2j',
+	mongoURI: 'mongodb://nickeduncan:Hihakiya1!@ds163630.mlab.com:63630/nick-emaily-dev',
+	cookieKey: 'qwerdlkgjsfrbbtwrtytnio'
+}
