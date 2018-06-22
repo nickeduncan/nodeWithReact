ad139951562adee7b7d1196f3a78257733e5772f
Nicholas Duncan <nickeduncan@gmail.com>
2018-06-22 14:59:30 -0400

    [8-91] added stripe keys

--- config/dev.js
+++ config/dev.js
@@ -3,5 +3,7 @@ module.exports = {
 	googleClientID: '339143140949-qa883l7l6jbktrisqojo7v5fn87eraf6.apps.googleusercontent.com',
 	googleClientSecret: 'pheqSN1PQFpjz2x6M2586D2j',
 	mongoURI: 'mongodb://nickeduncan:Hihakiya1!@ds163630.mlab.com:63630/nick-emaily-dev',
-	cookieKey: 'qwerdlkgjsfrbbtwrtytnio'
+	cookieKey: 'qwerdlkgjsfrbbtwrtytnio',
+	stripePublishableKey: 'https://github.com/BreakthroughBehavioralInc/Stern/pull/3327/files',
+	stripeSecretKey: 'sk_test_1tozc9SUeBtPyReEVLdoSIpN'
 }
