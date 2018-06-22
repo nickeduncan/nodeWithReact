186e5a292e4c39dfdfe323ac6fb281b415a878bd
Nicholas Duncan <nickeduncan@gmail.com>
2018-06-22 15:21:14 -0400

    [8-95] received token from stripe

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,9 +10,8 @@ class Header extends Component {
 						Emaily
 					</a>
 					<ul className="right">
-						<li>
-							<a>Login With Google</a>
-						</li>
+						<li><Payments /></li>
+						<li><a>Login With Google</a></li>
 					</ul>
 				</div>
 			</nav>
